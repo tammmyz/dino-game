b18bab098f4c3368005d9c1336fa86315aad6640
Sheridan Fong <sheri@LAPTOP13.intra.digitalembrace.ca>
2022-02-21 23:35:54 -0500

    seperated bird into it's own class and updated the main file

---
+++ source/bird.py
@@ -0,0 +1,18 @@
+from obstacle import Obstacle
+import random
+import pygame
+
+class Bird(Obstacle):
+    BIRD_HEIGHTS = [250, 290, 320]
+
+    def __init__(self, image):
+        self.type = 0
+        super().__init__(image, self.type)
+        self.rect.y = random.choice(self.BIRD_HEIGHTS)
+        self.index = 0
+
+    def draw(self, SCREEN):
+        if self.index >= 9:
+            self.index = 0
+        SCREEN.blit(self.image[self.index // 5], self.rect)
+        self.index += 1
--- source/chromedino.py
+++ source/chromedino.py
@@ -13,6 +13,7 @@ import time
 from cloud import Cloud
 from large_cactus import LargeCactus
 from small_cactus import SmallCactus
+from bird import Bird
 from obstacle import Obstacle
 import global_var
 import images
@@ -212,20 +213,20 @@ class Dinosaur:
 #         self.rect.y = 300
 
 
-class Bird(Obstacle):
-    BIRD_HEIGHTS = [250, 290, 320]
-
-    def __init__(self, image):
-        self.type = 0
-        super().__init__(image, self.type)
-        self.rect.y = random.choice(self.BIRD_HEIGHTS)
-        self.index = 0
-
-    def draw(self, SCREEN):
-        if self.index >= 9:
-            self.index = 0
-        SCREEN.blit(self.image[self.index // 5], self.rect)
-        self.index += 1
+# class Bird(Obstacle):
+#     BIRD_HEIGHTS = [250, 290, 320]
+#
+#     def __init__(self, image):
+#         self.type = 0
+#         super().__init__(image, self.type)
+#         self.rect.y = random.choice(self.BIRD_HEIGHTS)
+#         self.index = 0
+#
+#     def draw(self, SCREEN):
+#         if self.index >= 9:
+#             self.index = 0
+#         SCREEN.blit(self.image[self.index // 5], self.rect)
+#         self.index += 1
 
 
 def main():
