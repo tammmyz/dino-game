a54c1b9427c4b3a706ad706b6210b3c340d0384c
ChelseaMaramot <maramotc@mcmaster.ca>
2022-03-28 21:10:36 -0400

    Moved images in display to images.py

--- source/testing/test_instructions.py
+++ source/testing/test_instructions.py
@@ -1,11 +1,14 @@
 import unittest
+import sys, os
+
+
+sys.path.insert(1, os.path.abspath('.'))
 
 from display import *
-
+import global_var
 
 class TestInstructions(unittest.TestCase):
-    pass
-
+    pass 
 
 
 
